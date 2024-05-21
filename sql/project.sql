81f82e744fdeda89cfa8f5ae10639b31068f6142
Hwayeong <joid1004@naver.com>
2024-05-21 12:11:27 +0900

    BoardService, BoardMapper 작성
    board 테이블 생성 쿼리

---
+++ sql/project.sql
@@ -0,0 +1,11 @@
+create schema prj3;
+use prj3;
+
+CREATE TABLE board
+(
+    id       INT PRIMARY KEY AUTO_INCREMENT,
+    title    VARCHAR(100)  NOT NULL,
+    content  VARCHAR(1000) NOT NULL,
+    writer   VARCHAR(100)  NOT NULL,
+    inserted DATETIME      NOT NULL DEFAULT NOW()
+)
--- src/main/java/com/prj2spring20240521/domain/board/Board.java
+++ src/main/java/com/prj2spring20240521/domain/board/Board.java
@@ -1,5 +1,6 @@
 package com.prj2spring20240521.domain.board;
 
+
 public class Board {
     private String title;
     private String content;
