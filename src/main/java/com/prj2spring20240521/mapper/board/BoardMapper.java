9535a940cedc9b007d5796c3c0f995b8e41c91e3
Hwayeong <joid1004@naver.com>
2024-05-28 11:10:36 +0900

    첨부된 이미지 파일 수 출력

--- src/main/java/com/prj2spring20240521/mapper/board/BoardMapper.java
+++ src/main/java/com/prj2spring20240521/mapper/board/BoardMapper.java
@@ -42,8 +42,8 @@ public interface BoardMapper {
 
     @Select("""
             <script>
-            SELECT b.id, b.title, m.nick_name writer 
-            FROM board b JOIN member m ON b.member_id = m.id 
+            SELECT b.id, b.title, m.nick_name writer, COUNT(f.name) number_of_images 
+            FROM board b JOIN member m ON b.member_id = m.id LEFT JOIN board_file f ON b.id=f.board_id
             <trim prefix="WHERE" prefixOverrides="OR">
                 <if test="searchType != null">
             <bind name="pattern" value="'%'+keyword+'%'"/>    
@@ -56,6 +56,7 @@ public interface BoardMapper {
                  </if>
               </if>
             </trim>
+            GROUP BY b.id
             ORDER BY b.id DESC
             LIMIT #{offset},10
             </script>
